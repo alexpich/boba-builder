6900c640548c986b8440dd382ec6c3d5f9e43cc3
Alex <pich.alex01@gmail.com>
2020-06-01 21:05:39 -0700

    Added prop type validation

--- src/components/Boba/BobaIngredient/BobaIngredient.js
+++ src/components/Boba/BobaIngredient/BobaIngredient.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import classes from "./BobaIngredient.module.css";
 
-const bobaIngredient = (props) => {
-  let ingredient = null;
+class BobaIngredient extends Component {
+  render() {
+    let ingredient = null;
 
-  switch (props.type) {
-    case "cup":
-      ingredient = <div className={classes.Cup}></div>;
-      break;
-    case "aloe-vera":
-      ingredient = <div className={classes.AloeVera}></div>;
-      break;
-    case "boba":
-      ingredient = <div className={classes.Boba}></div>;
-      break;
-    case "egg-pudding":
-      ingredient = <div className={classes.EggPudding}></div>;
-      break;
-    case "grass-jelly":
-      ingredient = <div className={classes.GrassJelly}></div>;
-      break;
-    case "milk-tea":
-      ingredient = <div className={classes.MilkTea}></div>;
-      break;
-    default:
-      ingredient = null;
+    switch (this.props.type) {
+      case "cup":
+        ingredient = <div className={classes.Cup}></div>;
+        break;
+      case "aloe-vera":
+        ingredient = <div className={classes.AloeVera}></div>;
+        break;
+      case "boba":
+        ingredient = <div className={classes.Boba}></div>;
+        break;
+      case "egg-pudding":
+        ingredient = <div className={classes.EggPudding}></div>;
+        break;
+      case "grass-jelly":
+        ingredient = <div className={classes.GrassJelly}></div>;
+        break;
+      case "milk-tea":
+        ingredient = <div className={classes.MilkTea}></div>;
+        break;
+      default:
+        ``;
+        ingredient = null;
+    }
+
+    return ingredient;
   }
+}
 
-  return ingredient;
+BobaIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
-export default bobaIngredient;
+export default BobaIngredient;
