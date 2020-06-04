227bc357ecec464d316e768fbc02f9add5abcf44
Alex <pich.alex01@gmail.com>
2020-06-03 21:18:54 -0700

    More button adds items

--- src/containers/BobaBuilder/BobaBuilder.js
+++ src/containers/BobaBuilder/BobaBuilder.js
@@ -4,19 +4,40 @@ import Aux from "../../hoc/Aux";
 import Boba from "../../components/Boba/Boba";
 import BuildControls from "../../components/Boba/BuildControls/BuildControls";
 
+const INGREDIENT_PRICES = {
+  bobaPearl: 0.6,
+  eggPudding: 0.7,
+};
+
 class BobaBuilder extends Component {
   state = {
     ingredients: {
       bobaPearl: 0,
       eggPudding: 0,
     },
+    totalPrice: 9,
   };
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = (type) => {};
 
   render() {
     return (
       <Aux>
         <Boba ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
